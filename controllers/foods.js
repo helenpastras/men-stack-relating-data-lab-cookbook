5a9ff2312a8b2712d4d5affa5331f3c0172d1a87
helenpastras <helenapastras@hotmail.com>
2025-09-13 00:43:50 -0400

    add and index complete

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,6 +3,35 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-// router logic will go here - will be built later on in the lab
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('foods/index.ejs', {
+            user: currentUser
+            // foods: currentUser.pantry
+        });
+    } catch (error) {
+        console.log (error)
+        res.redirect('/')
+    }
+});
 
+router.get('/new', (req,res) => {
+    res.render('foods/new.ejs');
+})
+
+router.post('/', async (req,res) => {
+  try {
+      const currentUser = await User.findById(req.session.user._id);
+        console.log(req.body);
+        currentUser.pantry.push(req.body);
+    // Save changes to the user
+        await currentUser.save();
+
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 module.exports = router;
